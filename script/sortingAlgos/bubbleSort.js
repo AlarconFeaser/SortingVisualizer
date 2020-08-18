5f739a6dab39bf2aa688e63a1b5c225f968d01ca
Paul Alarcon <pa.f.e@hotmail.com>
2020-08-18 14:02:02 -0400

    added some responsiveness to canvas and the algorithms pages

--- script/sortingAlgos/bubbleSort.js
+++ script/sortingAlgos/bubbleSort.js
@@ -1,13 +1,16 @@
 import * as util from "../utils/util.js";
-import * as Canvas from "../utils/canvas.js";
+import * as canvas from "../utils/canvas.js";
 import * as modal from "../utils/modal.js";
 
 
-let arr = util.generateArray(25, 50);
-let originY = Canvas.height - 20;
-let width = Canvas.width / arr.length;
-let spacing = width * 0.1;
-let heightScale = Canvas.height * 0.1;
+let canvas_width = canvas.context.canvas.width;
+let canvas_height = canvas.context.canvas.height;
+
+let arr = util.generateArray(25, 20);
+let originY = canvas_height - canvas_height*.2;
+let width = canvas_width / arr.length;
+let spacing = canvas_height / arr.length * 0.1;
+let heightScale = canvas_height * 0.1;
 let margin = spacing / 2;
 let canvasArr = [];
 
@@ -15,40 +18,41 @@ const activeButtons = () => {
   let finalIteration = canvasArr.length - 1;
   let i = finalIteration;
 
+  canvas.context.font = '48px serif';
+  canvas.context.fillText(`Current Step : ${i} Total Steps : ${finalIteration}`, 10,  canvas_height - 50);
+
   const buttons = document.querySelectorAll(".control");
   buttons.forEach((b) => {
     b.addEventListener("click", () => {
       if (b.id == "prevButton") {
         if (i - 1 > 0) {
-          // console.log(finalIteration)
-          // Canvas.paintCanvas("black");
-          // console.log(canvasArr[0]);
-          let temp = --i;
-          drawArray(canvasArr[temp]);
-          console.log(temp);
+          drawArray(canvasArr[--i]);
         }
       } else if (b.id == "nextButton") {
-        if (i <= finalIteration) {
-          // Canvas.paintCanvas("black");
+        if (i <= finalIteration - 1) {
           drawArray(canvasArr[++i]);
         }
       } else if (b.id == "finalButton") {
         i = finalIteration;
-        // Canvas.paintCanvas("black");
         drawArray(canvasArr[i]);
       }
+
+    let font_size = Number(canvas_height*.05);
+    console.log(font_size);
+    canvas.context.font = `${font_size}px serif`;
+    canvas.context.fillText(`Current Step : ${i} Total Steps : ${finalIteration}`, 10,  canvas_height - canvas_height*.05);
+      
     });
   });
 };
 
 const drawArray = (arr) => {
-  //Repaint canvas everytime
-  Canvas.paintCanvas("black");
+  canvas.paintCanvas("black");
   arr.forEach((element, index) => {
     let posX = index * width + margin;
     let rectHeight = heightScale + element * 10;
-    Canvas.ctx.fillStyle = "white";
-    Canvas.ctx.fillRect(
+    canvas.context.fillStyle = "white";
+    canvas.context.fillRect(
       posX,
       originY - rectHeight,
       width - spacing,
@@ -69,6 +73,7 @@ const bubbleSort = arr => {
   }
 };
 
+let x = 0;
 const drawOnCanvas = (arr3d, speed) => {
   let index = 0;
   const currentInterval = setInterval(() => {
@@ -83,7 +88,6 @@ const drawOnCanvas = (arr3d, speed) => {
 const Main  = () => {
   //TOP SPEED
   let speed = 1000 / arr.length/2;
-
   bubbleSort(arr);
   drawOnCanvas(canvasArr, speed);
   activeButtons();
--- script/utils/canvas.js
+++ script/utils/canvas.js
@@ -1,25 +1,31 @@
-const canvas = document.querySelector(".myCanvas");
-const ctx = canvas.getContext("2d");
-const width = canvas.width;
-const height = canvas.height;
+const context = document.querySelector(".myCanvas").getContext("2d");
+// const context = canvas.getContext("2d");
+
+const render = () => {
+  context.canvas.width = document.documentElement.clientWidth*.8;
+  context.canvas.height = document.documentElement.clientHeight*.7;
+  paintCanvas();
+}
+
+window.addEventListener("resize", () => {
+  render();
+})
 
 const paintCanvas = (color) => {
-  ctx.fillStyle = color;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = color;
+  context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 };
 
-const renderImage = function(blob){
-  
+const renderImage = function(blob){  
   var img = new Image();
-
   img.onload = function(){
     ctx.drawImage(img, 0, 0)
   }
-
   img.src = URL.createObjectURL(blob);
 };
 
+render();
 
 export {
-  canvas, ctx, paintCanvas, width, height, renderImage
+ context, paintCanvas, renderImage
 }
